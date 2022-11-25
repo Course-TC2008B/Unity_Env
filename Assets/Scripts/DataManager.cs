1098d56f490ee8e77452d9dcd3ebbb4ca2e2c4f1
Jose Naime <josep.naimeg@gmail.com>
2022-11-25 17:40:28 -0600

    Avenue fix and code refactoring

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -3,83 +3,54 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+
 //
-public class DataManager : MonoBehaviour
-{
-//     
-    [SerializeField] 
-    private  Carro[] _cars;
+public class DataManager : MonoBehaviour {
+    //     
+    [SerializeField] private Carro[] _cars;
+
     //private   Semaforo[] _trafic_lights;
     private GameObject[] _carrosGO = null;
 
     //private GameObject[] _semaforosGO;
-    // private JsonManager _jsonManager;
 
-//
-//     // Start is called before the first frame update
-    void Start()
-    {
-        //_jsonManager = new JsonManager();
-        //_listSims = _jsonManager._listSimJson
-        print("DataManager Start _lenght: " + _cars.Length);
-        //_carrosGO = new GameObject[_cars.Length];
-        //_semaforosGO = new GameObject[_trafic_lights.Length];
-        
-        //for(int i = 0; i < _carrosGO.Length ; i++)
-        //{
-        //    _carrosGO[i] = CarPoolManager.Instance.ActivarObjeto(Vector2.zero);
-       // }
-        
-        //print("Carros_Go: " + _carrosGO.Length);
+    //
+    //     // Start is called before the first frame update
 
-        //PosicionarCarros();
-    }
-
-//
-    private void PosicionarCarros()
-    {
-        print("Carros Go: " + _carrosGO);
+    //
+    private void PosicionarCarros(){
         // Iniciar Cars
-        if (_carrosGO == null || _carrosGO.Length == 0)
-        {
+        if (_carrosGO == null || _carrosGO.Length == 0){
             print("Iniciar carros");
             _carrosGO = new GameObject[_cars.Length];
             print("PosicionarCarros carrosGo Lenght: " + _carrosGO.Length);
-            
-            for(int i = 0; i < _carrosGO.Length ; i++)
-            {
+            for (int i = 0; i < _carrosGO.Length; i++){
                 _carrosGO[i] = CarPoolManager.Instance.ActivarObjeto(Vector2.zero);
             }
         }
-            // Finalizar cars
+        // Finalizar cars
 
-            print("PosicionarCarros _lenght: " + _cars.Length);
-          for (int cr = 0; cr < _cars.Length; cr++)
-            {
-                float _x = _cars[cr].position[0];
-                float _y = _cars[cr].position[1];
-                
-                print("Car:" + cr + "  " + _x + " / " + _y);
-                _carrosGO[cr].transform.position = new Vector3(
-                    _x,
-                        0,
-                        _y
-                    );
-            
-                // print(_car[cr].position);
-            }
+        print("Carros Go: " + _carrosGO.Length);
+        print("PosicionarCarros _lenght: " + _cars.Length);
+        for (int cr = 0; cr < _cars.Length; cr++){
+            float _x = _cars[cr].position[0];
+            float _y = _cars[cr].position[1];
+            _carrosGO[cr].transform.position = new Vector3(_x,
+                                                           0,
+                                                           _y);
+        }
 
-            // for (int cr = 0; cr < _step[st].traffic_lights.Length; cr++)
-            // {
-            //     string key = _step[st].traffic_lights[cr].Keys.First();
-            //     _semaforosGO[cr] = CarPoolManagerSM.Instance.ActivarObjeto(
-            //
-            //         _step[st].traffic_lights[cr][key].state
-            //
-            //     );
-            //
-            // }
-            
+        // for (int cr = 0; cr < _step[st].traffic_lights.Length; cr++)
+        // {
+        //     string key = _step[st].traffic_lights[cr].Keys.First();
+        //     _semaforosGO[cr] = CarPoolManagerSM.Instance.ActivarObjeto(
+        //
+        //         _step[st].traffic_lights[cr][key].state
+        //
+        //     );
+        //
+        // }
+
         // for(int i = 0; i < _carros.Length; i++)
         // {
         //     _carrosGO[i].transform.position = new Vector3(
@@ -88,57 +59,24 @@ public class DataManager : MonoBehaviour
         //         _carros[i].z
         //     );
         // }
-
     }
 
-    void Update()
-    {
-
-
-    }
-
-    //
-//     // Update is called once per frame
-//     void Update()
-//     {
-//         if(Input.GetKeyDown(KeyCode.R)){
-//
-//             // simulando un update en datos
-//             for(int i = 0; i < _carros.Length; i++){
-//                 _carros[i].x = Random.Range(0f, 10f);
-//                 _carros[i].y = Random.Range(0f, 10f);
-//                 _carros[i].z = Random.Range(0f, 10f);
-//             }
-//
-//             PosicionarCarros();
-//         }
-//     }
-//
-    public void EscucharRequestSinArgumentos()
-    {
-
-        print("HUBO UN REQUEST MUY INTERESANTE!");
-    }
-
-    public void EscucharRequestConArgumentos(ListSim datos)
-    {
+    public void EscucharRequestConArgumentos(ListSim datos){
         print("DATOS: " + datos);
-
-        // actualizar arreglo _carros de esta clase con 
-        // los carros que recibo de "datos"
-
-        // invocar PosicionarCarros()
+        
         StartCoroutine(ConsumirSteps(datos));
     }
-    private IEnumerator ConsumirSteps(ListSim datos) {
-        for(int i = 0; i < datos.steps.Length - 1; i++){
+
+    private IEnumerator ConsumirSteps(ListSim datos){
+        for (int i = 0; i < datos.steps.Length - 1; i++){
             print("Step: " + i);
             _cars = datos.steps[i].cars;
             print("ConsumirSteps cars: " + _cars.Length);
             //_carros = datos.steps[i].carros;
-           // _trafic_lights = datos.steps[i].traffic_lights;
+            // _trafic_lights = datos.steps[i].traffic_lights;
             PosicionarCarros();
-            yield return new WaitForSeconds(0.5f);
+
+            yield return new WaitForSeconds(0.25f);
         }
     }
 }
--- Assets/Scripts/JsonManager.cs
+++ Assets/Scripts/JsonManager.cs
@@ -1,71 +1,45 @@
-
- using System;
- using System.Collections;
- using System.Collections.Generic;
- using System.Linq;
- using UnityEngine;
+using System.Collections;
+using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.Networking;
 
 [System.Serializable]
-public class RequestConArgumentos : UnityEvent<ListSim> { }
- public class JsonManager : MonoBehaviour{
+public class RequestConArgumentos : UnityEvent<ListSim> {
+}
 
-    [SerializeField]
-    private UnityEvent _requestRecibidaSinArgumentos;
+public class JsonManager : MonoBehaviour {
 
-    [SerializeField]
-    private RequestConArgumentos _requestConArgumentos;
+    [SerializeField] private UnityEvent _requestRecibidaSinArgumentos;
 
-    [SerializeField]
-    private string _url = "http://127.0.0.1:5000/run";
+    [SerializeField] private RequestConArgumentos _requestConArgumentos;
 
-    
+    [SerializeField] private string _url = "http://127.0.0.1:5000/run";
 
-    //public ListSim _listSimJson;
-
-    void Awake()
-    {
+    void Awake(){
         StartCoroutine(HacerRequest());
     }
 
-    IEnumerator HacerRequest()
-    {
-      
-            // hacer request al "server"
-            // esto va a cambiar mañana
-            // string jsonSource = PseudoServer.Instance.JSON;
-            UnityWebRequest www = UnityWebRequest.Get(_url);
-            yield return www.SendWebRequest();
-            string jsonSource = null;
-            if (www.result != UnityWebRequest.Result.Success)
-            {
-                print("ERROR EN REQUEST!");
-            }
-            else
-            {
-                jsonSource = www.downloadHandler.text;
-            }
-            print(jsonSource);
-            if (jsonSource != null)
-            {
-                ListSim listSim=
-                    JsonUtility.FromJson<ListSim>(jsonSource);
-                // for (int st = 0; st < listSim.steps.Length; st++)
-                // {
-                //     for (int ls = 0;  ls < listSim.steps[st].cars.Length; ls++)
-                //     {
-                //         print(listSim.steps[st].cars[ls].Keys.First());
-                //     }
-                //     for (int tf = 0;  tf < listSim.steps[st].traffic_lights.Length; tf++)
-                //     {
-                //         print(listSim.steps[st].cars[tf].Keys.First());
-                //     }
-                // }
-                _requestRecibidaSinArgumentos?.Invoke();
-                _requestConArgumentos?.Invoke(listSim);
-            }
+    IEnumerator HacerRequest(){
+        UnityWebRequest www = UnityWebRequest.Get(_url);
+
+        yield return www.SendWebRequest();
+        string jsonSource = null;
+        if (www.isNetworkError || www.isHttpError){
+            Debug.Log(www.error);
+        }
+
+        if (www.result != UnityWebRequest.Result.Success){
+            print("ERROR EN REQUEST!");
+        } else{
+            jsonSource = www.downloadHandler.text;
+        }
+
+        print(jsonSource);
+        if (jsonSource != null){
+            ListSim listSim =
+                JsonUtility.FromJson<ListSim>(jsonSource);
+            _requestConArgumentos?.Invoke(listSim);
+        }
     }
-    
+
 }
-     
