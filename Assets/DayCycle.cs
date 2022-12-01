b6381d6fb61fd8e407609610a23875f47dbbe41b
JoseNaime <josep.naimeg@gmail.com>
2022-12-01 16:43:03 -0600

    simulate daycycle toggle added

--- Assets/DayCycle.cs
+++ Assets/DayCycle.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class DayCycle : MonoBehaviour
 {
     [SerializeField] private float speed = 0.5f;
+    [SerializeField] private bool simulateDayTime = true;
 
     // Update is called once per frame
     void Update()
     {
+        if (simulateDayTime){
         transform.Rotate(new Vector3(1,0,0), Time.deltaTime * speed);
+        }
+    }
+
+    public void setSimulateDayTime()
+    {
+        simulateDayTime = !simulateDayTime;
     }
 }
